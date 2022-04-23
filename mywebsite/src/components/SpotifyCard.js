bd739c009469acd1b106e08a299b237378398c1d
pizza-hunter <hunter.green95@hotmail.com>
2022-04-23 18:57:18 +0930

    Unfinished future addition

---
+++ mywebsite/src/components/SpotifyCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class SpotifyCard extends React.Component {
+  state = {
+    song: "",
+    artist: "",
+    albumnImage: "",
+    link: "",
+  };
+  render() {
+    return <div>Spotify stuff..</div>;
+  }
+}
+//https://developer.spotify.com/documentation/web-api/quick-start/
