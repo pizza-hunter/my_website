0eb15c6f4e8bd571524e0710ebc88655a7dd5d9e
pizza-hunter <hunter.green95@hotmail.com>
2022-08-12 09:02:43 +0930

    Renamed to ButtonLink for reusability

--- mywebsite/src/components/ButtonLink.js
+++ mywebsite/src/components/ButtonLink.js
@@ -1,4 +1,4 @@
-const FileButtonLink = (props) => {
+const ButtonLink = (props) => {
   return (
     <a href={props.link} target="_blank" rel="noreferrer" className="font-bold">
       <button className="border-2 border-basic-h font-bold rounded-md px-4 py-2 text-basic-h  hover:bg-basic-l hover:border-basic-y hover:bg-opacity-20 duration-100">
@@ -8,4 +8,4 @@ const FileButtonLink = (props) => {
   );
 };
 
-export default FileButtonLink;
+export default ButtonLink;
