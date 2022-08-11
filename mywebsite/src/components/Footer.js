4429b76a4cecee954eb64876c3434c745867e934
pizza-hunter <hunter.green95@hotmail.com>
2022-08-12 09:25:40 +0930

    Footer component

---
+++ mywebsite/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="text-center text-xs p-5 mt-10 text-opacity-100 text-basic-l">
+        <p className="">
+          Designed and developed by Hunter Green, using React and Tailwind
+        </p>
+        <p className="pt-1">© Hunter Green</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
