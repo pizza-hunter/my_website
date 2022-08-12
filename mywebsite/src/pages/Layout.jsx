347c9ddee6cf335813c77a3f01a18ccb86c1ef20
pizza-hunter <hunter.green95@hotmail.com>
2022-08-12 09:37:46 +0930

    Footer added to common layout

--- mywebsite/src/pages/Layout.jsx
+++ mywebsite/src/pages/Layout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-// import "../styles/css/index.css";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
-      {/*TODO: Create <Footer /> */}
+      <Footer />
     </>
   );
 };
