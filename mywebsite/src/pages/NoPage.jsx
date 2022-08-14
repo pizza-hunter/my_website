d827ddc9fa2b4c5fe3d7b978985af84232c516b4
pizza-hunter <hunter.green95@hotmail.com>
2022-08-14 22:43:18 +0930

    404 Page message

--- mywebsite/src/pages/NoPage.jsx
+++ mywebsite/src/pages/NoPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function NoPage() {
-    return (
-        <h1>404</h1>
-    );
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 bg-basic-l p-10 rounded-lg">
+        <h1 className="text-4xl">
+          Uh Oh! 404! Looks like the page you requested doesn't exist!
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default NoPage;
