435122c92f5eaeed2b99d55afe3bba102412bb6e
pizza-hunter <hunter.green95@hotmail.com>
2022-12-18 16:25:20 +1030

    Footer text change

--- mywebsite/src/components/Footer.js
+++ mywebsite/src/components/Footer.js
@@ -4,9 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="text-center text-xs p-5 mt-10 text-opacity-100 text-basic-l">
-        <p className="">
-          Designed and developed by Hunter Green, using React and Tailwind
-        </p>
+        <p className="">Sticky taped together using React and Tailwind</p>
         <p className="pt-1">© Hunter Green</p>
       </div>
     );
