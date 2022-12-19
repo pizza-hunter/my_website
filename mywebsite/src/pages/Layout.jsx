54ebe3c1879e5b7caf0f4a0e591d9954333cded3
pizza-hunter <hunter.green95@hotmail.com>
2022-12-19 22:01:09 +1030

    Socials side link bar

--- mywebsite/src/pages/Layout.jsx
+++ mywebsite/src/pages/Layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import LinkBar from "../components/LinkBar";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
+      <LinkBar />
       <Outlet />
       <Footer />
     </>
