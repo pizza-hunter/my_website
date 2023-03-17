5841be7478a1b3aa52565cd923b1b18f47de5909
pizza-hunter <hunter.green95@hotmail.com>
2023-03-17 23:23:15 +1030

    Updating to named props

--- mywebsite/src/components/ContentBlock.js
+++ mywebsite/src/components/ContentBlock.js
@@ -1,19 +1,22 @@
 import React from "react";
 
-function ContentBlock(props) {
-  const blockC =
-    props.blockClasses !== undefined ? props.blockClasses : "pt-10";
+function ContentBlock({
+  blockClasses,
+  headerClasses,
+  textClasses,
+  header,
+  text,
+}) {
+  const blockC = blockClasses !== undefined ? blockClasses : "pt-10";
   const headerC =
-    props.headerClasses !== undefined
-      ? props.headerClasses
-      : "text-basic-a text-3xl";
-  const textC = props.textClasses !== undefined ? props.textClasses : "pt-8";
+    headerClasses !== undefined ? headerClasses : "text-basic-a text-3xl";
+  const textC = textClasses !== undefined ? textClasses : "pt-8";
 
   return (
     <div className={blockC}>
-      <div className={props.blockClasses}>
-        <h1 className={headerC}>{props.header}</h1>
-        <p className={textC}>{props.text}</p>
+      <div className={blockClasses}>
+        <h1 className={headerC}>{header}</h1>
+        <p className={textC}>{text}</p>
       </div>
     </div>
   );
